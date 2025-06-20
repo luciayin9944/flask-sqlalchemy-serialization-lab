5590564cc4a315097a2d764c138d3db569743834
luciayin9944 <luxiayin94@gmail.com>
2025-06-20 14:27:42 -0700

    final solution

--- server/models.py
+++ server/models.py
@@ -17,8 +17,18 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item') ##[item.item for item in customer.reviews]
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
+    
+class CustomerSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.String()
+
+    reviews = fields.List(fields.Nested(lambda:ReviewSchema(exclude=("customer",))))
+    # reviews = fields.Nested(lambda:ReviewSchema(exclude=("customer",)))
 
 
 class Item(db.Model):
@@ -28,5 +38,40 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates='item')
+    customers = association_proxy("reviews", "customer") 
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+    
+class ItemSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.String()
+    price = fields.Float()
+    
+    reviews = fields.List(fields.Nested(lambda:ReviewSchema(exclude=("item",))))
+    # review = fields.Nested(lambda:ReviewSchema)
+
+#step 1
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, Customer {self.customer_id}, Item {self.item_id}>'
+
+class ReviewSchema(Schema):
+    id = fields.Int(dump_only=True)
+    comment = fields.String()
+
+    customer = fields.Nested(lambda:CustomerSchema(exclude=("reviews",)))
+    item = fields.Nested(lambda:ItemSchema(exclude=("reviews",)))
+
